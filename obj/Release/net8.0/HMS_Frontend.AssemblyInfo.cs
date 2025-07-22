36257e542e770892fbc9dc1a1cd7b2fe606b4589
Sridhar <2431425@cognizant.com>
2025-07-22 10:44:18 +0530

    m

--- obj/Release/net8.0/HMS_Frontend.AssemblyInfo.cs
+++ obj/Release/net8.0/HMS_Frontend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS_Frontend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f216e95e7c1dc58206e702c44e810cd975d4a2b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS_Frontend")]
